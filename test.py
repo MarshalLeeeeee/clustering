1cf7646508d1e327170c7cc8d29e15ee4e7411af
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-03-19 13:12:31 +0800

    modify test

--- test.py
+++ test.py
@@ -59,16 +59,19 @@ if __name__ == '__main__':
 	
 	m = [[0,0],[-5,5],[5,-5],[5,5]];
 	s = [[[1,-0.2],[-0.2,1]],[[1,0],[0,1]],[[1,0],[0,1]],[[1,-0.8],[-0.8,1]]]
-	data, miu, Sigma, partial = init(2,1000,4,s = s[:4], m = m[:4])
+	m1 = [[0,4], [2,-2], [-2,-2]]
+	s1 = [[[1,0],[0,1]],[[1,0],[0,1]],[[1,0],[0,1]]]
+	kp1000 = [333,333,334]
+	data, miu, Sigma, partial = init(2,1000,3,s = s1, m = m1, kp = kp1000)
 
-	#cluster = Kmeans.k_means(data, 4, 1000, Debug = True, debug = 1, slow = True)
+	cluster = Kmeans.k_means(data, 4, 1000, Debug = True, debug = 1, slow = True, competitive = True, alpha = 150)
 	#print("The cluster is: ")
 	#print(cluster)
 	#print(data)
 	#np.seterr(all='raise')
 
 	
-	cluster_miu, cluster_Sigma, cluster_pi = GMM.gmm(data, 4, 100, Debug = True, debug = 1, independent = True)
+	#cluster_miu, cluster_Sigma, cluster_pi = GMM.gmm(data, 4, 100, Debug = True, debug = 1, independent = True)
 	#print(cluster_miu)
 	#print(cluster_Sigma)
 	#print(cluster_pi)
